7e0386d700a5277d3af2f6de03a7e259f22e61a3
Spre <Spre@8f7cbc4e-d821-48fb-b665-6de9fca9247a>
2008-03-18 00:40:32 +0000

    -Rev 14- [2008/3/16] Spre - Modified Alpha Mob DB Code, removed some possible unneeded Rows. Changes atk1&2 around. -

--- Branches/SQL/MySQL/Mob_database_Structure_Alpha.sql
+++ Branches/SQL/MySQL/Mob_database_Structure_Alpha.sql
@@ -1,18 +1,18 @@
- CREATE TABLE `mobs` (
-  `ID` mediumint(9) unsigned NOT NULL default '0',
+
+CREATE TABLE IF NOT EXISTS `mobs` (
   `Name` text NOT NULL,
   `aRan` text NOT NULL,
   `LV` text NOT NULL,
   `HP` tinyint(6) unsigned NOT NULL default '0',
   `SP` int(9) unsigned NOT NULL default '0',
   `str` mediumint(9) unsigned NOT NULL default '0',
-  ``int`` mediumint(9) unsigned NOT NULL default '0',
+  `int` mediumint(9) unsigned NOT NULL default '0',
   `vit` mediumint(9) unsigned NOT NULL default '0',
   `dex` tinyint(4) unsigned NOT NULL default '0',
   `agi` smallint(6) unsigned NOT NULL default '0',
-  `lik` smallint(6) unsigned NOT NULL default '0',
-  `atk1` smallint(6) unsigned NOT NULL default '0',
-  `atk2` smallint(6) unsigned NOT NULL default '0',
+  `luk` smallint(6) unsigned NOT NULL default '0',
+  `attack_rating_min` smallint(6) unsigned NOT NULL default '0',
+  `attack_rating_max` smallint(6) unsigned NOT NULL default '0',
   `def` tinyint(4) unsigned NOT NULL default '0',
   `exp` tinyint(4) unsigned NOT NULL default '0',
   `jexp` tinyint(4) unsigned NOT NULL default '0',
@@ -27,6 +27,5 @@
   `race` smallint(6) unsigned NOT NULL default '0',
   `mdef` smallint(6) unsigned NOT NULL default '0',
   `taming_item` smallint(6) unsigned NOT NULL default '0',
-  `food_item` smallint(6) unsigned NOT NULL default '0',
-   PRIMARY KEY  (`ID`)
+  `food_item` smallint(6) unsigned NOT NULL default '0'
 ) TYPE=MyISAM;
