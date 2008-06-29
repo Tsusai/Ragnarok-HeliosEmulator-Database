8d2c609c36a3e8163fbdb2a63eaef80aeb4b5d7b
Spre <Spre@8f7cbc4e-d821-48fb-b665-6de9fca9247a>
2008-06-29 02:33:48 +0000

    -Rev 26- [2008/6/28] Spre - Same as revision 25 but for SQLITE, any issues leet me know or revert. -

---
+++ Trunk/SQL/SQLite/Updates/upgrade_revision25.sql
@@ -0,0 +1,46 @@
+--Update adding quests, auction etc. Used eAthena as the source.
+
+-----------------------------------------------------------------------------
+-- Quests
+-----------------------------------------------------------------------------
+
+CREATE TABLE [quests]
+(
+  [id] INTEGER NOT NULL PRIMARY KEY, --AUTO_INCREMENT
+  [char_id] INTEGER NOT NULL PRIMARY KEY,
+ );
+ 
+CREATE TABLE [quest_reqs]
+(
+  `id` INTEGER NOT NULL PRIMARY KEY,
+  `quest_count` INTEGER NOT NULL,
+  `quest_name` VARCHAR(255),
+  `quest_num` INTEGER NOT NULL PRIMARY KEY,
+  `char_id` INTEGER NOT NULL PRIMARY KEY,
+);
+
+-----------------------------------------------------------------------------
+-- Auction
+-----------------------------------------------------------------------------
+
+CREATE TABLE [auction] 
+(
+  [auction_id] INTEGER NOT NULL PRIMARY KEY,
+  [seller_id] INTEGER NOT NULL,
+  [seller_name] VARCHAR(30),
+  [buyer_id] INTEGER NOT NULL,
+  [buyer_name] VARCHAR(30),
+  [price] INTEGER NOT NULL,
+  [buynow] INTEGER NOT NULL,
+  [hours] INTEGER NOT NULL,
+  [timestamp] INTEGER NOT NULL,
+  [nameid] INTEGER NOT NULL,
+  [item_name] INTEGER NOT NULL,
+  [type] INTEGER NOT NULL,
+  `[refine] INTEGER NOT NULL,
+  `[attribute] INTEGER NOT NULL,
+  `[card0] INTEGER NOT NULL,
+  `[card1] INTEGER NOT NULL,
+  `[card2] INTEGER NOT NULL,
+  `[card3] INTEGER NOT NULL,
+);
